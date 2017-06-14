fc824120f18311f160310483c5647b5fa2ccb668
ganlvtech <ganlvtech@qq.com>
2017-06-14 20:49:53 +0800

    Add: visit log

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'log_path' => dirname(__FILE__) . '/../runtime/log',
+);
--- public/index.php
+++ public/index.php
@@ -98,6 +98,27 @@ class MyPhpCasProxy extends PhpCasProxy
     }
 }
 
+// 记录文件
+$config = include '../config/config.php';
+if ($config['log_path']) {
+    $path_with_date = $config['log_path'] . '/' . date('Ym');
+    if (!file_exists($path_with_date)) {
+        $oldmask = umask(0);
+        mkdir($path_with_date, 0777, true);
+        umask($oldmask);
+    }
+    $log_file = $path_with_date . '/' . date('d') . '.log';
+    /**
+     * @var string $full_url 当前页面完整网址
+     *
+     * @link https://stackoverflow.com/questions/6768793/get-the-full-url-in-php/6768831#6768831
+     */
+    $full_url = (isset($_SERVER['HTTPS']) ? 'https' : 'http') ."://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+    $datetime = date(DateTime::ATOM);
+    $log_content = "[$datetime] $full_url\n";
+    file_put_contents($log_file, $log_content, FILE_APPEND);
+}
+
 // 主程序
 $cas_config = include '../config/cas.php';
 $phpCasProxy = new MyPhpCasProxy($cas_config['server'], 'is_available_host', $cas_config['my_service'], $cas_config['my_cas_context']);
